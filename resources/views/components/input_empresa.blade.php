ef04b324da6fd7c312d975e7e894cec9f52375e2
Wilson-Rojas <117527439+Wilson-Rojas@users.noreply.github.com>
2024-06-12 12:03:21 -0400

    Avances en EVENTOS EMPRESAS

--- resources/views/components/input_empresa.blade.php
+++ resources/views/components/input_empresa.blade.php
@@ -2,5 +2,5 @@
 
 <div class="relative w-full max-w-xs">
     <label for="{{ $attributes->get('id') }}" class="absolute -top-2 left-0 bg-white w-full px-1 text-xs text-gray-600 transition-all duration-200">{{ $label }}</label>
-    <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'block w-full px-4 py-2 text-gray-900 placeholder-transparent border-b border-gray-300 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:border transition-all duration-200']) !!}>
+    <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md  block w-full px-4 py-2 text-gray-900 placeholder-transparent border-b border-gray-300 focus:outline-none focus:border-verde-800 focus:ring-1 focus:ring-verde-800 focus:border transition-all duration-200']) !!}>
 </div>
